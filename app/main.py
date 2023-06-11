6de6d14f56ddf54fdf366fedcea781bf09bee19c
Du4nG <dung@Silly>
2023-06-11 13:31:13 +0700

    add docker, sudo docker-compose up -d

--- app/main.py
+++ app/main.py
@@ -3,10 +3,23 @@ from . import models
 from .database import engine
 from .routers import post, user, auth, vote
 from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    '*',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 my_posts = [{'id':1,'title':'title of post 1', 'content':'content of post 1'}, 
             {'id':2,'title':'title of post 2', 'content':'content of post 2'}] 
